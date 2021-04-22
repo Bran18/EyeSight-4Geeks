d4f9dc6777c20cda9de59934dcf0e6974b199889
jenkins96 <adrianjnkns@gmail.com>
2021-04-22 02:08:02 +0000

    Created HomeDemo component, css to it and added some comments in css

---
+++ src/front/js/component/HomeDemo.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const HomeDemo = () => {
+	return (
+		<div className="container">
+			<div className="row d-flex justify-content-center mb-5">
+				<div className="col col-xs-12">
+					<img
+						className="img-thumbnail rounded"
+						src="https://dummyimage.com/250x250/000/fff"
+						alt="Thumbnail image"
+					/>
+				</div>
+				<div className="col col-xs-12">
+					<img
+						className="img-thumbnail rounded"
+						src="https://dummyimage.com/250x250/000/fff"
+						alt="Thumbnail image"
+					/>
+				</div>
+				<div className="col col-xs-12">
+					<img
+						className="img-thumbnail rounded"
+						src="https://dummyimage.com/250x250/000/fff"
+						alt="Thumbnail image"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomeDemo;
