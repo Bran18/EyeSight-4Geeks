6adc722bb524fda9159a89364715d55afe6a87d7
jenkins96 <adrianjnkns@gmail.com>
2021-04-22 03:15:07 +0000

    Added Link en HomeDemo and some styling in CSS

--- src/front/js/component/HomeDemo.js
+++ src/front/js/component/HomeDemo.js
@@ -1,29 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeDemo = () => {
 	return (
 		<div className="container">
+			<h2 className=" text-center mb-5 home__demo__title">Learn With Your Favorite Image!</h2>
 			<div className="row d-flex justify-content-center mb-5">
-				<div className="col col-xs-12">
-					<img
-						className="img-thumbnail rounded"
-						src="https://dummyimage.com/250x250/000/fff"
-						alt="Thumbnail image"
-					/>
+				<div className="col">
+					<Link to="/">
+						<a href="#">
+							<img
+								className="img-thumbnail "
+								src="https://dummyimage.com/310x220/000/fff"
+								alt="Thumbnail image"
+							/>
+						</a>
+					</Link>
 				</div>
-				<div className="col col-xs-12">
-					<img
-						className="img-thumbnail rounded"
-						src="https://dummyimage.com/250x250/000/fff"
-						alt="Thumbnail image"
-					/>
+				<div className="col">
+					<Link to="/">
+						<a href="#">
+							<img
+								className="img-thumbnail "
+								src="https://dummyimage.com/310x220/000/fff"
+								alt="Thumbnail image"
+							/>
+						</a>
+					</Link>
 				</div>
-				<div className="col col-xs-12">
-					<img
-						className="img-thumbnail rounded"
-						src="https://dummyimage.com/250x250/000/fff"
-						alt="Thumbnail image"
-					/>
+				<div className="col">
+					<Link to="/">
+						<a href="#">
+							<img
+								className="img-thumbnail "
+								src="https://dummyimage.com/310x220/000/fff"
+								alt="Thumbnail image"
+							/>
+						</a>
+					</Link>
 				</div>
 			</div>
 		</div>
